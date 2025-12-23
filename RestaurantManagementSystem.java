525b04db03b7f13ef3b67197e2e4889bb793fa32
techwithjoshi66 <techwithjoshi2025@gmail.com>
2025-12-23 12:52:36 +0530

    Initialize variables for total costs

--- RestaurantManagementSystem.java
+++ RestaurantManagementSystem.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class RestaurantManagementSystem {
-    
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        //Initialize variables for total costs
+        double tamt = 0.0;//Total cost for Starters
+        double tamt1 = 0.0;//Total cost for Main Course
+        double tamt3 = 0.0;//Total cost for Desserts
+
+        double tot = 0.0;//Grand total food cost(tamt + tamt1 + tamt2)
+        double gst = 0.0; //Calculated GST 
+        double gtotal = 0.0;//Final amount to be paid(tot + gst)
+    }
 }
