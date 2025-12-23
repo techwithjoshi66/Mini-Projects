f0a37b94103d3800dfc83c9fd8d93f1f56cae5b0
techwithjoshi66 <techwithjoshi2025@gmail.com>
2025-12-23 18:33:13 +0530

    Variables for order logic and Variables for order quantities and items numbers.

--- RestaurantManagementSystem.java
+++ RestaurantManagementSystem.java
@@ -11,5 +11,22 @@ public class RestaurantManagementSystem {
         double tot = 0.0;//Grand total food cost(tamt + tamt1 + tamt2)
         double gst = 0.0; //Calculated GST 
         double gtotal = 0.0;//Final amount to be paid(tot + gst)
+
+        //Variables for order logic
+        String choice1 = "";
+        String choice = "Y";
+        String str = "";
+
+        //Variables for order quantities and items numbers
+        int vstr =0;//Veg Starter item number
+        int tvstr = 0;//veg starter quantity
+        int nvd = 0;//Non-veg starter item number
+        int tnvstr =0;//Non-veg starter quantity 
+        int vfd =0;//Main Course item quantity
+        int tvfd =0;//Main Course quantity
+        int d = 0;//Desert item number
+        int totald = 0;//Dessert Quantity
+
+        double amt =0.0;//Temporary variable to store cost of current item * Quantity
     }
 }
